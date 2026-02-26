bd65549e445119167ee1d98e15215b4c5baa2fbc
jhon david vanegas <jhonva0986@gmail.com>
2026-02-26 17:05:22 -0500

    se hicieron algunos cambios en las tarjetas de productos, se cambio el logo y se agrego la seccion recetas

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,29 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Productos from "./pages/Productos";
+import Recetas from "./pages/Recetas";
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
+import Cart from "./pages/Cart";
 import ProtectedRoute from "./router/ProtectedRoute";
 
 function App() {
   return (
-    <div className="layout">
+    <>
       <Navbar />
-
-      <main className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/productos" element={<Productos />} />
-          <Route path="/login" element={<Login />} />
-
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute>
-                <Admin />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </main>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/recetas" element={<Recetas />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/admin" element={
+          <ProtectedRoute>
+            <Admin />
+          </ProtectedRoute>
+        }/>
+      </Routes>
+    </>
   );
 }
 
